257d2c8c5afc373fe5b2bd29ad4d34d597802353
Samuel Kane <samuel.l.kane@gmail.com>
2022-12-22 20:40:11 -0500

    feat: PlayerSimulator improvements
    Makes simulation style based off of number.
    
    Adds beginning support for smart hand choices

--- five_card/PlayerSimulator.py
+++ five_card/PlayerSimulator.py
@@ -2,12 +2,13 @@
 import random
 from Player import Player
 from pydealer import Deck
+from HandAnalyzer import HandAnalyzer
 
-def simulatePlayer(player: Player, deck: Deck ,difficulty='easy',):
+def simulatePlayer(player: Player, deck: Deck ,difficulty='1',):
     difficultyMap = {
-        'easy': simulateEasy,
-        'medium': simulateMedium,
-        'hard': simulateHard
+        '1': simulateEasy,
+        '2': simulateMedium,
+        '3': simulateHard
     }
     difficultyMap[difficulty](player, deck)
 
@@ -19,15 +20,23 @@ def simulateEasy(player: Player, deck: Deck):
             
 
 def simulateMedium(player: Player, deck: Deck):
+    analyzer = HandAnalyzer()
     """Medium players understand strategy and will analyze their hand but still behave randomly"""
     for position in range(5):
-        if random.randrange(2^5) == 0:
-            #TODO: Add intelligent swapping here using hand analyzer
+        if random.randrange(2^3) == 0:
+            currentHandRank = analyzer.calculate(player.getHand())
+            if(currentHandRank[0] < 'a-royal-flush'):
+                #TODO: Add intelligent swapping here using hand analyzer
+                pass
             player.swapCard(deck.deal(1),position)
 
 def simulateHard(player: Player, deck: Deck):
+    analyzer = HandAnalyzer()
     """Hard players will try and optimize their hands with zero randomness"""
     for position in range(5):
-        #TODO: Add intelligent swapping here using hand analyzer
+        currentHandRank = analyzer.calculate(player.getHand())
+        if(currentHandRank[0] < 'a-royal-flush'):
+            #TODO: Add intelligent swapping here using hand analyzer
+            pass
         player.swapCard(deck.deal(1),position)
     simulateEasy(player, deck)
